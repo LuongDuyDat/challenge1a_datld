cbcdef596ef69656ea112989289fdcfac7d48943
LuongDuyDat <luongduydat123@gmail.com>
2024-01-04 15:04:30 +0700

    add redirect to path / in web

--- routes.php
+++ routes.php
@@ -4,8 +4,9 @@ require 'core/router.php';
 
 $router = new Router();
 
-$router->get('/', 'controller/login_controller.php');
-$router->post('/', 'controller/login_controller.php');
+$router->get('/', 'controller/index_controller.php');
+$router->get('/login', 'controller/login_controller.php');
+$router->post('/login', 'controller/login_controller.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
