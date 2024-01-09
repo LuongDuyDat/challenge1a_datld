11990d80e924aad7ab47a4175ca12815cd01acc9
LuongDuyDat <luongduydat123@gmail.com>
2024-01-09 18:19:09 +0700

    update db

--- db/learn_management.sql
+++ db/learn_management.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: localhost    Database: learn_management
 -- ------------------------------------------------------
--- Server version	8.0.28
+-- Server version	8.0.35-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +28,7 @@ CREATE TABLE `account` (
   `password` varchar(45) NOT NULL,
   `role` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'teacher1','123456a@A',0),(2,'teacher2','123456a@A',0),(3,'student1','123456aA',1),(4,'student2','123456a@A',1),(17,'student3','123456',1);
+INSERT INTO `account` VALUES (1,'teacher1','123456a@A',0),(2,'teacher2','123456a@A',0),(3,'student1','123456aA',1),(4,'student2','123456a@A',1);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,8 +55,10 @@ CREATE TABLE `exercises` (
   `size` int NOT NULL,
   `teacher_id` int NOT NULL,
   `file_path` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  KEY `fk_exercises_1_idx` (`teacher_id`),
+  CONSTRAINT `fk_exercises_1` FOREIGN KEY (`teacher_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +67,7 @@ CREATE TABLE `exercises` (
 
 LOCK TABLES `exercises` WRITE;
 /*!40000 ALTER TABLE `exercises` DISABLE KEYS */;
-INSERT INTO `exercises` VALUES (2,'Bai tap Shell Script','Challenge-1 (1).pdf',214466,1,'assets/exercise/Challenge-1 (1).pdf'),(3,'Bai tap Web 1','Challenge-1a.docx',121546,1,'assets/exercise/Challenge-1a.docx'),(4,'Bai tap Web 2','Challenge-1a.docx',121546,2,'assets/exercise/Challenge-1a.docx'),(5,'Bai tap Web 3','Challenge-1a.docx',121546,1,'assets/exercise/Challenge-1a.docx');
+INSERT INTO `exercises` VALUES (2,'Bai tap Shell Script','Challenge-1a.docx',121546,1,'assets/exercise/1704782681-9290-Challenge-1a.docx'),(3,'Bai tap Web 1','Challenge-1a (1).docx',121546,1,'assets/exercise/1704782706-6615-Challenge-1a (1).docx'),(7,'Bai tap Web 2','Challenge-1 (1).pdf',214466,2,'assets/exercise/1704784665-8838-Challenge-1 (1).pdf');
 /*!40000 ALTER TABLE `exercises` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,8 +84,10 @@ CREATE TABLE `homework_files` (
   `file_path` text NOT NULL,
   `name` varchar(100) NOT NULL,
   `size` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  KEY `fk_homework_files_1_idx` (`homework_id`),
+  CONSTRAINT `fk_homework_files_1` FOREIGN KEY (`homework_id`) REFERENCES `homeworks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +96,7 @@ CREATE TABLE `homework_files` (
 
 LOCK TABLES `homework_files` WRITE;
 /*!40000 ALTER TABLE `homework_files` DISABLE KEYS */;
-INSERT INTO `homework_files` VALUES (1,1,'assets/homework/homework.pdf','homework.pdf',116166),(2,1,'assets/homework/homework.pdf','homework.pdf',116166),(3,2,'assets/homework/Bài tập 1 - Lương Duy Đạt - 19020039.pdf','Bài tập 1 - Lương Duy Đạt - 19020039.pdf',141346),(4,2,'assets/homework/Bài tập 1 - Lương Duy Đạt - 19020039.pdf','Bài tập 1 - Lương Duy Đạt - 19020039.pdf',141346),(5,2,'assets/homework/Bài tập 1 - Lương Duy Đạt - 19020039.pdf','Bài tập 1 - Lương Duy Đạt - 19020039.pdf',141346),(6,3,'assets/homework/Bài tập 1 - Lương Duy Đạt - 19020039.pdf','Bài tập 1 - Lương Duy Đạt - 19020039.pdf',807997);
+INSERT INTO `homework_files` VALUES (26,12,'assets/homework/1704795172-3944-homework.pdf','homework.pdf',116166);
 /*!40000 ALTER TABLE `homework_files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,8 +111,12 @@ CREATE TABLE `homeworks` (
   `id` int NOT NULL AUTO_INCREMENT,
   `exercise_id` int NOT NULL,
   `student_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  KEY `fk_homeworks_1_idx` (`exercise_id`),
+  KEY `fk_homeworks_2_idx` (`student_id`),
+  CONSTRAINT `fk_homeworks_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_homeworks_2` FOREIGN KEY (`student_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +125,7 @@ CREATE TABLE `homeworks` (
 
 LOCK TABLES `homeworks` WRITE;
 /*!40000 ALTER TABLE `homeworks` DISABLE KEYS */;
-INSERT INTO `homeworks` VALUES (1,5,3),(2,5,4),(3,5,17);
+INSERT INTO `homeworks` VALUES (12,7,3);
 /*!40000 ALTER TABLE `homeworks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,10 +139,14 @@ DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
   `id` int NOT NULL AUTO_INCREMENT,
   `sender_id` int NOT NULL,
-  `receiver_id` varchar(45) NOT NULL,
+  `receiver_id` int NOT NULL,
   `content` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  KEY `fk_messages_1_idx` (`sender_id`),
+  KEY `fk_messages_2_idx` (`receiver_id`),
+  CONSTRAINT `fk_messages_1` FOREIGN KEY (`sender_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_messages_2` FOREIGN KEY (`receiver_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +155,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (11,1,'1','Tôi là Teacher 1!');
+INSERT INTO `messages` VALUES (11,1,1,'Tôi là Teacher 1!');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +172,8 @@ CREATE TABLE `profile` (
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(100) DEFAULT NULL,
   `avatar` text,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_profile_1` FOREIGN KEY (`id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -170,7 +183,7 @@ CREATE TABLE `profile` (
 
 LOCK TABLES `profile` WRITE;
 /*!40000 ALTER TABLE `profile` DISABLE KEYS */;
-INSERT INTO `profile` VALUES (1,'Teacher 1','teacher1@gmail.com','0123456789','assets/images/avatar-1.png'),(2,'Teacher 2','teacher2@gmail.com','0123456788','assets/images/default_avatar.jpg'),(3,'Student 1','student1@gmail.com','0123456787','assets/images/avatar-3.jpg'),(4,'Student 2','student2@gmail.com','0123456786','assets/images/default_avatar.jpg'),(17,'Student 3','student3@gmail.com','0123456788','assets/images/default_avatar.jpg');
+INSERT INTO `profile` VALUES (1,'Teacher 1','teacher1@gmail.com','0123456789','assets/images/avatar-1.png'),(2,'Teacher 2','teacher2@gmail.com','0123456788','assets/images/default_avatar.jpg'),(3,'Student 1','student1@gmail.com','0123456787','assets/images/avatar-3.jpg'),(4,'Student 2','student2@gmail.com','0123456786','assets/images/default_avatar.jpg');
 /*!40000 ALTER TABLE `profile` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -183,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-08 22:03:32
+-- Dump completed on 2024-01-09 18:17:53
