bebc4ca0548ddc9a1a6f5aefda60af2ee648be07
LuongDuyDat <luongduydat123@gmail.com>
2024-01-10 15:20:18 +0700

    add css to homeview

--- view/home_view.php
+++ view/home_view.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <?php require base_path("controller/partition/header_controller.php")?>
-    <h1>Đây là trang chủ</h1>
+    <section class="main-content">
+        <h2>Đây là Trang chủ</h2>
+        <p>Đây là nơi chứa thông tin chính của trang chủ.</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Your Website. All rights reserved.</p>
+    </footer>
 </body>
 </html>
